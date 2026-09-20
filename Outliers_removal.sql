52284d2cd4a2d83c488f11c1d42bd76b61d63462
minghaochen666666-sketch <minghaochen666666@gmail.com>
2026-09-20 03:03:10 -0400

    Add files via upload

---
+++ Outliers_removal.sql
@@ -0,0 +1,18 @@
+with standard as(
+select
+	avg(price) as avg_price,
+    stddev(price) as std_dev_price
+from
+	modified_data
+)
+
+select
+	price,
+    bedrooms,
+    bathrooms,
+    sqft_living,
+    view
+from
+	modified_data,standard
+where
+	price between (avg_price - 3 * std_dev_price) and (avg_price + 3 * std_dev_price)
